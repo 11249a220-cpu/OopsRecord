29811ac33cd9b0ae0f7dddae1867799ab2cbe58d
11249a220-cpu <11249a220@kanchiuniv.ac.in>
2025-11-05 20:46:14 +0530

    Create HospitalPatientRecord.cpp

---
+++ HospitalPatientRecord.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Patient{
+    public:
+    int patientID;
+    string name;
+    string disease;
+    // function defined inside the class
+    void setPatient(int id,string n,string d){
+        patientID=id;
+        name=n;
+        disease=d;
+    }
+    // function declared inside,defined outside
+    void showPatient();
+};
+// function defined outside the class
+void Patient::showPatient(){
+    cout<<"Patient ID:"<<patientID
+    <<",Name"<<name
+    <<",Disease:"<<disease<<endl;
+}
+int main(){
+    Patient p1,p2;
+    // using inside function to set details
+    p1.setPatient(301,"Arjun","Fever");
+    p2.setPatient(302,"navvu","Diabetes");
+    // using outside function to display
+    p1.showPatient();
+    p2.showPatient();
+    return 0;
+}
+    
